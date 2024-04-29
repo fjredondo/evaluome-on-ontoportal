9862ac44c56af9c5ba7542c1a33624887dd2bc09
fjredondo <franciscojavier.redondoa@um.es>
2024-04-29 23:44:15 +0200

    OntologySet section update

--- 1-ontologySet/ontoportal_download.sh
+++ 1-ontologySet/ontoportal_download.sh
@@ -1,13 +1,39 @@
 #! /bin/bash
 
+if [[ $# -ne 2 ]]; then
+	echo "Please provide two arguments a csv file path and download folder path."
+	exit 1
+fi
+
+repo_list=$1
+folder=$2
+
+# Verify that the csv file has been provided as an argument.
+if [[ ! -f ${repo_list} ]]; then
+    echo "Please provide the csv file as an argument with this header: Repository,APIKey."
+    exit 1
+fi
+
+if [[ ! -d ${folder} ]]; then
+    echo "Please provide a valid folder to download ontologies."
+    exit 1
+fi
+
+# Output file with the list of ontologies
+ontology_list="${folder}/ontology_list.csv"
+
+rm -f "${ontology_list}"
+
 {
+
+    echo "Repository,Ontology,CreationDate,Format,URL" >> ${ontology_list}
     IFS=, read -r -a header
 
     while IFS=, read -r "${header[@]}" ; do
         repo="https://data.${Repository}"
         auth="Authorization: apikey token=${APIKey}"
-	rm -rf "./${Repository}"
-	mkdir "./${Repository}"
+	rm -rf "${folder}/${Repository}"
+	mkdir "${folder}/${Repository}"
 
         while read submission
 	do
@@ -17,11 +43,12 @@
 		ontoName=$(echo "$urlSub"|cut -d '/' -f 5)
 		fn=$(echo "$ontoName-$creationDate.${ontoFormat}" | awk '{print tolower($0)}')
 		urlDownload=$(echo "$urlSub/download")
-		filePath=$(echo "./${Repository}/${fn}")
+		filePath=$(echo "${folder}/${Repository}/${fn}")
 
-  		echo "$Repository - $filePath - ${urlDownload}"
+  		echo "${folder}/${Repository} - ${filePath} - ${urlDownload}"
+		echo "${Repository},${ontoName},${creationDate},${ontoFormat},${urlSub}" >> ${ontology_list}
 
-		wget -O "$filePath" -P "$Repository" --header "$auth" "$urlDownload"
+		wget -O "${filePath}" -P "${folder}/${Repository}" --header "${auth}" "$urlDownload"
 
 	done < <(curl --location "$repo/submissions?display=hasOntologyLanguage%2Cstatus%2CcreationDate%2Cversion%2CsubmissionId&display_links=false&display_context=false" \
                --header 'Content-Type: application/json' \
@@ -30,4 +57,4 @@
 
 
     done
-} < repo_list.csv
+} < ${repo_list}
