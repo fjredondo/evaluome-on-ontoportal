c710d037632ebb2db728d591b54ef104a0f220ca
fjredondo <franciscojavier.redondoa@um.es>
2024-04-30 18:10:47 +0200

    OntologySet section update

--- 1-ontologySet/ontoportal_download.sh
+++ 1-ontologySet/ontoportal_download.sh
@@ -21,12 +21,15 @@ fi
 
 # Output file with the list of ontologies
 ontology_list="${folder}/ontology_list.csv"
+# Download error file
+download_err="${folder}/ontology_err.csv"
 
 rm -f "${ontology_list}"
-
+rm -f "${download_err}"
 {
 
-    echo "Repository,Ontology,CreationDate,Format,URL" >> ${ontology_list}
+    echo "Repository,Ontology,CreationDate,Format,Length,URL" >> ${ontology_list}
+    echo "Repository,Ontology,CreationDate,Format,HTTPResponse,URL" >> ${download_err}
     IFS=, read -r -a header
 
     while IFS=, read -r "${header[@]}" ; do
@@ -46,9 +49,17 @@ rm -f "${ontology_list}"
 		filePath=$(echo "${folder}/${Repository}/${fn}")
 
   		echo "${folder}/${Repository} - ${filePath} - ${urlDownload}"
-		echo "${Repository},${ontoName},${creationDate},${ontoFormat},${urlSub}" >> ${ontology_list}
 
-		wget -O "${filePath}" -P "${folder}/${Repository}" --header "${auth}" "$urlDownload"
+		wgetOut=$(wget --server-response -q -O "${filePath}" -P "${folder}/${Repository}" --header "${auth}" "$urlDownload"  2>&1|egrep "HTTP|Length")
+
+		httpResponse=$(echo "$wgetOut" | grep "HTTP/" | awk '{print $2}')
+		length=$(echo "$wgetOut" | grep "Content-Length" | awk '{print $2}')
+		if [ $httpResponse == "200" ]; then
+			echo "${Repository},${ontoName},${creationDate},${ontoFormat},${length},${urlSub}" >> ${ontology_list}
+		else
+			echo "${Repository},${ontoName},${creationDate},${ontoFormat},${httpResponse},${urlSub}" >> ${download_err}
+		fi
+
 
 	done < <(curl --location "$repo/submissions?display=hasOntologyLanguage%2Cstatus%2CcreationDate%2Cversion%2CsubmissionId&display_links=false&display_context=false" \
                --header 'Content-Type: application/json' \
